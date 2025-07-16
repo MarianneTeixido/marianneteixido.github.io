66b4d5cdb7e28f7bb3b30f9878997cfd878f9d59
mw-msct <ztp.txdo@gmail.com>
2025-07-16 15:58:17 -0600

    [feat] scroll smooth effect

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
-  <div id="app" :class="{ 'sidebar-is-open': isMenuOpen }">
-    <Sidebar :is-open="isMenuOpen" @close="isMenuOpen = false" class="sticky"/>
-    <div class="main-container">
-      <Header @toggle-menu="toggleMenu" class="sticky"/>
-      <main class="main-content">
-        <router-view class="sticky"/>
-      </main>
+  <div id="smooth-wrapper">
+    <div id="smooth-content">
+      <div id="app" :class="{ 'sidebar-is-open': isMenuOpen }">
+        <Sidebar :is-open="isMenuOpen" @close="isMenuOpen = false" class="sticky" />
+        <div class="main-container">
+          <Header @toggle-menu="toggleMenu" class="sticky" />
+          <main class="main-content">
+            <router-view class="sticky" />
+          </main>
+        </div>
+      </div>
     </div>
-    <StickyCursor />
   </div>
+  <StickyCursor />
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Sidebar from './components/Sidebar.vue'
 import Header from './components/Header.vue'
 import StickyCursor from './components/StickyCursor.vue'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+onMounted(() => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 1.5,
+    effects: true,
+  });
+});
 
 const isMenuOpen = ref(false)
 const toggleMenu = () => {
